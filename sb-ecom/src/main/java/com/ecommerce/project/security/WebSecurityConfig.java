a30d20495c47613df5650fcf21b8bee94a4484b0
Vishesh <vishesh98sharma@gmail.com>
2025-06-04 19:54:28 -0400

    Deployment

--- sb-ecom/src/main/java/com/ecommerce/project/security/WebSecurityConfig.java
+++ sb-ecom/src/main/java/com/ecommerce/project/security/WebSecurityConfig.java
@@ -85,6 +85,7 @@ public class WebSecurityConfig {
                                 .requestMatchers("/swagger-ui/**").permitAll()
                                 .requestMatchers("/api/test/**").permitAll()
                                 .requestMatchers("/images/**").permitAll()
+                                .requestMatchers("/api/send-message").permitAll()
                                 .anyRequest().authenticated()
                 );
 
